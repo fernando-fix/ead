e4c642c10e0f1d276dda40fd712a780cc8b2e1d5
Fernando Rodrigues <fernando_tecti@hotmail.com>
2023-05-01 15:49:15 -0300

    criado dois templates para aulas e cursos

--- script.sql
+++ script.sql
@@ -18,3 +18,26 @@ CREATE TABLE logs (
   description VARCHAR(255) NOT NULL,
   created_at DATETIME NOT NULL
 );
+
+-- Criar tabela courses
+CREATE TABLE courses (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(75) NOT NULL,
+  logo VARCHAR(255) NOT NULL
+);
+
+-- Criar tabela modules
+CREATE TABLE modules (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(75) NOT NULL,
+  course_id INT NOT NULL
+);
+
+-- Criar tabela lessons
+CREATE TABLE lessons (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(75) NOT NULL,
+  order_number INT NOT NULL,
+  module_id int NOT NULL,
+  course_id int NOT NULL
+);
